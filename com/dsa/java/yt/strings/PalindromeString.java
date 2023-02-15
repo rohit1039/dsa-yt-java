7df61c259b3845e9c3d9ba5b54260168df5d021c
rohit1039 <rohitparida05992gmail.com>
2023-02-15 08:31:02 +0530

    strings

--- com/dsa/java/yt/strings/PalindromeString.java
+++ com/dsa/java/yt/strings/PalindromeString.java
@@ -1,23 +1,36 @@
 package com.dsa.java.yt.strings;
 
+/**
+ * @author - ROHIT PARIDA
+ */
 public class PalindromeString {
 
     public static void main(String[] args) {
 
-        String s1 = "MALAYALAM"; // true
+        String s1 = "WGGL"; // false
         String s2 = "AMERICA"; // false
-        boolean flag = checkPalindrome(s1);
+        String s3 = "MALAYALAM"; // true
+        boolean flag = checkPalindrome(s3);
         System.out.println(flag);
     }
 
+    /**
+     *
+     * @param s1
+     * @return
+     */
     private static boolean checkPalindrome(String s1) {
+
+        boolean flag = s1.length() == 0 || s1.length() == 1;
 
         int start = 0;
         int end = s1.length() - 1;
-        boolean flag = false;
 
         while (start < end) {
             flag = s1.charAt(start) == s1.charAt(end);
+            if (!flag) {
+                break;
+            }
             start++;
             end--;
         }
